f9ebb9d1192455408bb688f69dcb0cd53937dd36
dinesh-platformatory <dinesh@platformatory>
2021-09-11 01:36:34 +0530

    bug fixes

--- add_data.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-data_directory=/opt/app/data
-mkdir -p ${data_directory}
-echo "=> File created at `data`" | tee ${data_directory} create.log
-
-
-# Provide it execution  permission
-# Then see created log at /opt/app/data/create.log
